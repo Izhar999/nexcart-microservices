656051314e1acd266c04ecdf7e04762b594c7a19
Izhar999 <izharulhassan7781@gmail.com>
2026-06-07 11:48:04 +0530

    Add custom user model and PostgreSQL integration

---
+++ services/user-service/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+  ROLE_CHOICES = (
+    ('admin', 'Admin'),
+    ('customer','Customer')
+  )
+
+  email=models.EmailField(unique=True)
+  role=models.CharField(max_length=20,choices=ROLE_CHOICES,default='customer')
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
+
+  def __str__(self):
+    return self.email
